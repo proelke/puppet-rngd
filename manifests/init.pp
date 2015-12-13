f4f76ea2ac63ef3475ced236f6f9306c9cdcdb2c
Phil Friderici <phil.friderici@i-tee.de>
2015-12-13 15:31:34 +0000

    Refactor code & spec tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,32 @@
 # == Class: rngd
 #
 class rngd (
-  $config          = 'USE_DEFAULTS',
+  $config_file     = 'USE_DEFAULTS',
   $config_template = 'rngd/rngd.erb',
-  $extra_options   = '',
+  $extra_options   = undef,
   $package_ensure  = 'present',
   $package_name    = 'USE_DEFAULTS',
   $service_enable  = true,
   $service_ensure  = 'running',
   $service_manage  = true,
   $service_name    = 'USE_DEFAULTS',
+  # deprecated variables
+  $config          = undef,
 ) {
 
+  # deprecations
+  if $config != undef {
+    notify { '*** DEPRECATION WARNING***: $config was renamed to $config_file. Please update your configuration. Support for $config will be removed in the near future!': }
+    $config_file_temp = $config
+  } else {
+    $config_file_temp = $config_file
+  }
+  # /deprecations
+
+  # osfamily deviation handling
   case $::osfamily {
     'RedHat': {
-      $config_default       = '/etc/sysconfig/rngd'
+      $config_file_default  = '/etc/sysconfig/rngd'
       $package_name_default = ['rng-tools']
       $service_name_default = 'rngd'
     }
@@ -23,16 +35,27 @@ class rngd (
     }
   }
 
-  if $config == 'USE_DEFAULTS' {
-    $config_real = $config_default
+  if $config_file_temp == 'USE_DEFAULTS' {
+    $config_file_real = $config_file_default
   } else {
-    $config_real = $config
+    $config_file_real = $config_file_temp
   }
 
   if $package_name == 'USE_DEFAULTS' {
-    $package_name_real = $package_name_default
+    $package_name_array = $package_name_default
   } else {
-    $package_name_real = $package_name
+    # convert $package_name to array to allow using a string
+    case type3x($package_name) {
+      'array': {
+        $package_name_array = $package_name
+      }
+      'string': {
+        $package_name_array = any2array($package_name)
+      }
+      default: {
+        fail('cron::package_name is not a string nor an array.')
+      }
+    }
   }
 
   if $service_name == 'USE_DEFAULTS' {
@@ -40,43 +63,60 @@ class rngd (
   } else {
     $service_name_real = $service_name
   }
+  # /osfamily specifics handling
 
-  validate_absolute_path($config_real)
+  # convert input variable types
+  $service_ensure_string = "${service_ensure}" # lint:ignore:only_variable_string
+  $service_enable_string = "${service_enable}" # lint:ignore:only_variable_string
+
+  if is_bool($service_manage) == true {
+    $service_manage_bool = $service_manage
+  } else {
+    $service_manage_bool = str2bool($service_manage)
+  }
+  # /convert input variable types
+
+  # validate variable content
+  validate_absolute_path($config_file_real)
+  validate_array($package_name_array)
+  validate_bool($service_manage_bool)
   validate_string($config_template)
-  validate_string($extra_options)
-  validate_string($package_ensure)
-  validate_array($package_name_real)
-  validate_bool($service_enable)
-  validate_string($service_ensure)
-  validate_bool($service_manage)
   validate_string($service_name_real)
-
-  if ! ($rngd::service_ensure in [ 'running', 'stopped' ]) {
-    fail('service_ensure parameter must be running or stopped')
+  if $extra_options != undef {
+    validate_string($extra_options)
   }
+  validate_re($package_ensure, '^(present|installed|absent|latest)$',
+    'rngd::service_ensure must be present, installed, absent or latest.')
+  validate_re($service_enable_string, '^(true|false|manual|mask)$',
+    'rngd::service_ensure must be true, false, manual or mask.')
+  validate_re($service_ensure_string, '^(running|stopped|true|false)$',
+    'rngd::service_ensure must be running, stopped, true or false.')
+  # /validate variable content
 
-  package { $rngd::package_name_real:
+  # configure resources
+  package { $rngd::package_name_array:
     ensure => $rngd::package_ensure,
+    before => File[$rngd::config_file_real],
   }
 
-  file { $rngd::config_real:
+  file { $rngd::config_file_real:
     ensure  => file,
     owner   => 0,
     group   => 0,
     mode    => '0644',
     content => template($rngd::config_template),
-    require => Package[$rngd::package_name_real],
   }
 
-  if $rngd::service_manage == true {
+  if $rngd::service_manage_bool == true {
     service { 'rngd':
-      ensure     => $rngd::service_ensure,
-      enable     => $rngd::service_enable,
-      name       => $rngd::service_name,
+      ensure     => $rngd::service_ensure_string,
+      enable     => $rngd::service_enable_string,
+      name       => $rngd::service_name_real,
       hasstatus  => true,
       hasrestart => true,
-      require    => File[$rngd::config_real],
-      subscribe  => File[$rngd::config_real],
+      require    => File[$rngd::config_file_real],
+      subscribe  => File[$rngd::config_file_real],
     }
   }
+  # /configure resources
 }
